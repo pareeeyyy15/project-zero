0b2302a98a102d1ab2510dc4501ec10bb4551aed
pareeeyyy15 <paripiku1506@gmail.com>
2026-02-23 21:16:35 +0530

    Add files via upload

---
+++ cexercise5.c
@@ -0,0 +1,88 @@
+// switch case statements 
+#include <stdio.h>
+
+//int main(){
+
+
+
+   // int age;
+   // printf("Enter your age\n");
+   // scanf("%d" , & age);
+
+   // switch (age)            
+
+   // {
+   // case 3 :
+  //      printf(" The age is 3\n");
+   //     break;
+
+   // case 13 :
+   //     printf(" The age is 13\n");
+   //     break;
+
+  //  case 23 :
+  //     printf(" The age is 23\n");
+   //     break;
+    
+   // default:
+  //  printf(" AGE IS NOT 3, 13 OR 23\n");
+        
+   // }
+  //    return 0 ;
+//} 
+
+
+int main() {
+
+
+
+int age, marks;
+
+
+    printf("Enter your age\n");
+    scanf("%d" , &age);
+
+
+    printf("Enter your marks\n");
+    scanf("%d" , & marks);
+
+    switch (age)            
+
+    {
+    case 3 :
+        printf(" The age is 3\n");
+        break;
+
+
+       switch (marks)
+       {
+         case 45:
+          printf("Your marks are 45");
+          break;
+       
+         default:
+         printf("Your marks are not 45");
+       
+
+
+
+        break;
+       
+    {
+                 {
+                    case 13 :
+                 printf(" The age is 13\n");
+                 break;
+
+
+                 case 23 :
+                 printf(" The age is 23\n");
+                 break;
+    
+               
+                      printf(" AGE IS NOT 3, 13 OR 23\n");
+        
+                  }
+                   }   return 0;
+                }
+     }  }      
