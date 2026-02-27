3d55152832d8659bdaee9cf4f70e50dccc94f54a
pareeeyyy15 <paripiku1506@gmail.com>
2026-02-27 21:45:36 +0530

    Add files via upload

---
+++ cexercie12.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//Typecasting syntax
+//(type) value;
+
+
+ int main () {
+
+ 
+
+//     int a =3;
+//     float b = 54;
+//     printf("The value of a is %d\n",  (int) b);
+//     return 0;
+//  }
+
+
+int a = 3;
+float  b = (float)54/5;
+printf("The value of a is %f\n" , b);
+return 0 ;
+ }
