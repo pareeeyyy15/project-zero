4d7789812aacee3e75c9c9c5d6a0924745f6c460
pareeeyyy15 <paripiku1506@gmail.com>
2026-03-10 20:34:38 +0530

    Add files via upload

---
+++ cexercise24.c
@@ -0,0 +1,33 @@
+//pointers  
+ 
+
+
+#include<stdio.h>
+
+int main () 
+{
+    // printf(" Lets learn about pointers\n");
+    // int a =76;
+    // int * ptra= &a;
+    // printf("The value of a is %d\n" , *ptra) ;
+    // return 0;
+
+    // printf(" Lets learn about pointers\n");
+    //  int a =76;
+    //  int * ptra= &a;
+    //  printf("The value of a is %x\n" , ptra) ;
+    //  return 0;
+
+
+
+     printf(" Lets learn about pointers\n");
+     int a =76;
+     int * ptra= &a;
+     printf("The address of pointer to  a is %p\n" , &ptra) ;
+      printf("The value of a is %p\n" ,  *ptra) ;
+     return 0;
+                                                                                    
+
+
+    
+} 
