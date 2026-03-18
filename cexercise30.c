2a28236186a963b0915f482d65671389c080f01b
pareeeyyy15 <paripiku1506@gmail.com>
2026-03-18 20:11:43 +0530

    Add files via upload

---
+++ cexercise30.c
@@ -0,0 +1,23 @@
+//strings 
+
+#include<stdio.h>
+int func1(int array[])
+{
+      for (int i = 0; i < 4; i++)
+      {
+        printf("The value at %d is %d\n" , i , array[i]);
+      }
+      
+       return 0;
+      
+      
+}
+
+int main() 
+{
+    int arr[] ={23, 13, 3,4};
+    printf("The value at index 0 is %d\n" , arr[0]);
+    func1(arr);
+    printf("The value at index 0 is %d\n" , arr[0]);
+    return 0;
+}
