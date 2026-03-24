d37d7ef82a5a35748e498199dd0741423fc7123a
pareeeyyy15 <paripiku1506@gmail.com>
2026-03-24 20:40:00 +0530

    Add files via upload

---
+++ cexercise34.c
@@ -0,0 +1,32 @@
+//typedef in c 
+
+
+#include<stdio.h>
+// struct Student
+// {
+//     int id;
+//     int marks;
+//     char fav_char;
+//     char name[34];
+// };
+
+int main() 
+{
+    
+
+
+    // struct Student s1, s2;
+    // s1.id= 34;
+    // s2.id= 56;
+    // printf("The value of s1's id is %d\n" , s1.id);
+    // printf("The value of s2's id is %d\n" , s2.id);
+    // typedef <previous_name> <alias_name>;
+    // typedef unsigned long ul;
+    // typedef int integer;
+    // ul l1, l2, l3;
+    // integer a = 6;
+    // printf("Value of a is %d" ,a );
+    return 0;
+
+
+}
